826183bda359d19e4c2f266d9a32052629cc3300
Sal-p <Personal@Sals-MBP.home>
2018-09-21 15:02:30 +1000

    export updated

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-'use strict'
-
-let plugin
-try {
-  plugin = require('./dist').default
-} catch (err) {
-  if (err.code === 'MODULE_NOT_FOUND') {
-    require('babel-register')
-    plugin = require('./src').default
-  } else {
-    console.log(err)
-    process.exit(1)
-  }
-}
+const { default: plugin } = require('./dist');
 
 module.exports = plugin
